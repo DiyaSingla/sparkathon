7580cd5bf7298493b6a2cca594a8d0fcce5821f8
Diya <diyasingla2004@gmail.com>
2023-08-15 16:25:30 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/sparkathon_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sparkathon_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page.dart
@@ -0,0 +1,23 @@
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:flutter/material.dart';
+// import 'package:sparkathon_app/api.dart';
+
+// class Items extends StatefulWidget {
+//   @override
+//   _ItemState createState() => _ItemState();
+// }
+
+// class _ItemState extends State<Items> {
+//   List<Dataset> Items = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     Dataset.fetchAll().then((data) {
+//       setState(() {
+//         Items = data;
+//       });
+//     });
+//   }
+// }
